6b1e7b482788e1a8a7c49796b0ea306c2a0a77e7
st3v3-t <st3v3-t@users.noreply.github.com>
2016-11-08 17:22:31 +0000

    Added server.js for Azure .

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var app = express()
+
+var transactions = []
+var futursism = {}
+
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.post('/transactions', function (req, res) {
+  console.log("Received " + req.body.length + " transactions")
+
+  req.body.map(function (item) {
+    transactions.push(item)
+    var date = Date.parse(item.transactionDateTime)
+    console.log(date.getMonth())
+  });
+
+  res.status(200).send()
+})
+
+app.get('/transactions', function(req, res) {
+  res.send(transactions)
+})
+
+app.get('/', function (req, res) {
+  res.status(200).json({hello: "world"})
+})
+
+app.listen(3000, '0.0.0.0', function () {
+  console.log('Example app listening on port 3000!')
+})
