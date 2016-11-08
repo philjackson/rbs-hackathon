0ac8652b58657c0b45203de99eb353c903cd67da
Phil Jackson <phil@shellarchive.co.uk>
2016-11-08 16:48:03 +0000

    Transaction reception.

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.status(200).json({hello: "world"});
+var transactions = []
+var futursism = {}
+
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.post('/transactions', function (req, res) {
+  console.log("Received " + req.body.length + " transactions")
+
+  req.body.map(function (item) {
+    transactions.push(item)
+    var date = Date.parse(item.transactionDateTime)
+    console.log(date.getMonth())
+  });
+
+  res.status(200).send()
 })
 
-app.listen(3000, function () {
+app.get('/transactions', function(req, res) {
+  res.send(transactions)
+})
+
+app.get('/', function (req, res) {
+  res.status(200).json({hello: "world"})
+})
+
+app.listen(3000, '0.0.0.0', function () {
   console.log('Example app listening on port 3000!')
 })
