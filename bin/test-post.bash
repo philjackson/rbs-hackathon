681973bf21a358b5a29bc36604b551b5e5c46938
David Carr <davidcarrs@gmail.com>
2016-11-09 13:03:13 +0000

    fixed up generation of seen transactions

--- bin/test-post.bash
+++ bin/test-post.bash
@@ -4,3 +4,33 @@ curl -H "Content-type: application/json" \
      -X "POST" \
      -d '[{"accountId": "57e3b951a746a0f62525f820", "transactionDateTime": "2015-02-22T20:58:26.512Z", "transactionAmount": -499.99, "accountBalance": 13186.43, "transactionType": "POS", "transactionDescription": "HARVEY NICHOLS EDINBURGH GB", "id": "57e3b9545fcd0537745f428f", "category": "clothing", "unfamiliar" : "false"}]' \
      'http://localhost:3000/transactions'
+
+curl -H "Content-type: application/json" \
+     -X "POST" \
+     -d '[{"accountId": "57e3b951a746a0f62525f820", "transactionDateTime": "2015-03-22T20:58:26.512Z", "transactionAmount": -499.99, "accountBalance": 13186.43, "transactionType": "POS", "transactionDescription": "HARVEY NICHOLS EDINBURGH GB", "id": "57e3b9545fcd0537745f428f", "category": "clothing", "unfamiliar" : "false"}]' \
+     'http://localhost:3000/transactions'
+     
+curl -H "Content-type: application/json" \
+     -X "POST" \
+     -d '[{"accountId": "57e3b951a746a0f62525f820", "transactionDateTime": "2015-04-22T20:58:26.512Z", "transactionAmount": -499.99, "accountBalance": 13186.43, "transactionType": "POS", "transactionDescription": "HARVEY NICHOLS EDINBURGH GB", "id": "57e3b9545fcd0537745f428f", "category": "clothing", "unfamiliar" : "false"}]' \
+     'http://localhost:3000/transactions'
+     
+curl -H "Content-type: application/json" \
+     -X "POST" \
+     -d '[{"accountId": "57e3b951a746a0f62525f820", "transactionDateTime": "2015-03-22T20:58:26.512Z", "transactionAmount": -499.99, "accountBalance": 13186.43, "transactionType": "POS", "transactionDescription": "TEST", "id": "57e3b9545fcd0537745f428f", "category": "clothing", "unfamiliar" : "false"}]' \
+     'http://localhost:3000/transactions'
+     
+curl -H "Content-type: application/json" \
+     -X "POST" \
+     -d '[{"accountId": "57e3b951a746a0f62525f820", "transactionDateTime": "2015-04-22T20:58:26.512Z", "transactionAmount": 100, "accountBalance": 13186.43, "transactionType": "POS", "transactionDescription": "TEST", "id": "57e3b9545fcd0537745f428f", "category": "clothing", "unfamiliar" : "false"}]' \
+     'http://localhost:3000/transactions'
+     
+curl -H "Content-type: application/json" \
+     -X "POST" \
+     -d '[{"accountId": "57e3b951a746a0f62525f820", "transactionDateTime": "2015-04-10T20:58:26.512Z", "transactionAmount": 100, "accountBalance": 13186.43, "transactionType": "POS", "transactionDescription": "TEST", "id": "57e3b9545fcd0537745f428f", "category": "clothing", "unfamiliar" : "false"}]' \
+     'http://localhost:3000/transactions'
+     
+curl -H "Content-type: application/json" \
+     -X "POST" \
+     -d '[{"accountId": "57e3b951a746a0f62525f820", "transactionDateTime": "2015-01-22T20:58:26.512Z", "transactionAmount": 100, "accountBalance": 13186.43, "transactionType": "POS", "transactionDescription": "TEST", "id": "57e3b9545fcd0537745f428f", "category": "clothing", "unfamiliar" : "false"}]' \
+     'http://localhost:3000/transactions'
