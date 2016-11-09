c3f2cca3f30d2a3ab3f221c0b7f068ac54b16b31
David Carr <davidcarrs@gmail.com>
2016-11-09 11:39:25 +0000

    added detection of transaction runs

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var config = require('./config/config')
 var dateFormat = require('dateformat')
 var app = express()
 
+var futureMonths = 3
 var transactions = []
+var future_transactions = []
 var future_store = {}
 var seen_payees = {}
+var monthly_transactions = {}
+var confidence_store_month = {}
+var runs
+var transactionInfo
 
 var bodyParser = require('body-parser')
 
@@ -37,10 +43,78 @@ function onItem(item) {
   transactions.push(item)
 }
 
+function calculateFutureStore(){
+  
+  confidence_store_month={}
+   
+  Object.keys(future_store).map(function(k) {
+    future_store[k].map(function(v) {
+      let date = new Date(v.transactionDateTime)
+      let year = dateFormat(date, "yyyy")
+      let month = dateFormat(date, "mm")
+      let day = dateFormat(date, "dd")
+      let monthCounter = ((year - 1900) * 12) + month - 1
+
+      if (! (monthCounter in confidence_store_month)) {
+        confidence_store_month[monthCounter] = {}
+      }
+      
+      if (! (v.transactionDescription in confidence_store_month[monthCounter])) {
+        confidence_store_month[monthCounter][v.transactionDescription] = {}
+      }
+      
+      confidence_store_month[monthCounter][v.transactionDescription] = {
+        accountId : v.accountId,
+        day : day,
+        transactionAmount : v.transactionAmount,
+        transactionType : v.transactionType,
+        transactionDescription : v.transactionDescription,
+        category : v.category
+      }
+    })
+  })
+
+  processMonthStore(confidence_store_month);
+}
+
+function processMonthStore(store){
+
+    if(Object.keys(store).length == 0)
+        return
+        
+    var sorted_months = Object.keys(store).sort().reverse();
+    let firstKey = sorted_months.shift()
+    runs = {}
+    transactionInfo = {}
+    
+    let seed = Object.keys(store[firstKey])
+    seed.forEach(function(des){
+        runs[des] = 1;
+        transactionInfo[des] = store[firstKey][des]
+    })
+    
+    sorted_months.forEach(function(month){
+        last = seed
+        Object.keys(store[month]).forEach(function(description){
+            if(last.indexOf(description)> -1){
+                runs[description] = runs[description] + 1
+            }
+        })
+        last = Object.keys(store[month])
+    })
+    
+    /*future_transactions={}
+    Object.keys(runs).forEach(function(description){
+        for(var month=firstKey+1; month < firstKey + futureMonths ; month++)
+        future_transactions.push(transactionInfo[des]
+    }*/
+}
+
 app.post('/transactions', function (req, res) {
   console.log("Received " + req.body.length + " transactions")
   console.log(req.query)
   req.body.map(onItem);
+  calculateFutureStore()
   res.status(200).send()
 })
 
@@ -49,26 +123,11 @@ app.get('/transactions/past', function(req, res) {
 })
 
 app.get('/transactions/future', function(req, res) {
-  let confidence_store_month = {}
-
-  Object.keys(future_store).map(function(k) {
-    future_store[k].map(function(v) {
-      let date = new Date(item.transactionDateTime)
-      let day = dateFormat(date, "dd")
-
-      if (! (day in confidence_store_month)) {
-        confidence_store_month[day] = {}
-      }
-
-      confidence_store_month[day][transactionDescription] = 0
-    })
-  })
-
-  res.send({})
+  res.send(futureTransactions)
 })
 
 app.get('/debug', function(req, res) {
-  res.send(future_store)
+  res.send(transactionInfo)
 })
 
 app.get('/', function (req, res) {
